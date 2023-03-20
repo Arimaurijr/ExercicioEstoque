e0437958fca203c05a42e896ec55ca662509c7d2
Arimaurijr <ari.mauri.jr@gmail.com>
2023-03-20 13:44:41 -0300

    Add project files.

---
+++ ExercicioLista/Produto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioLista
+{
+    internal class Produto
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+
+        public Produto(int id, string nome)
+        {
+            this.ID = id;
+            this.Nome = nome;
+        }
+        public override string ToString()
+        {
+            return "\nDados do PRODUTO:\nID: " + ID + "\nNome: " + Nome + "\n\n";
+        }
+    }
+}
---
+++ ExercicioLista/Program.cs
@@ -0,0 +1,40 @@
+﻿using ExercicioLista;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<Produto> estoque = new List<Produto>();
+
+        Console.WriteLine("Count: " + estoque.Count);
+        Console.WriteLine("Capacity: " + estoque.Capacity);
+
+        estoque.Add(new Produto(1,"Borracha"));
+       //Console.WriteLine("01 - Count: " + estoque.Count);
+        //Console.WriteLine("01 - Capacity: " + estoque.Capacity);
+
+        estoque.Add(new Produto(2, "Caderno"));
+        //Console.WriteLine("02 - Count: " + estoque.Count);
+        //Console.WriteLine("02 - Capacity: " + estoque.Capacity);
+
+        estoque.Add(new Produto(3, "Apontador"));
+        //Console.WriteLine("03 - Count: " + estoque.Count);
+        //Console.WriteLine("03 - Capacity: " + estoque.Capacity);
+
+        Console.WriteLine("\n\nElementos da Lista de Estoque ORDENADOS:");
+        List<Produto> estoqueordenado = estoque.OrderBy(Produto => Produto.Nome).ToList();
+
+        //foreach(Produto produto in estoque) 
+        for(int i = 0; i < estoqueordenado.Count; i++)
+        {
+            Console.WriteLine(estoqueordenado[i].ToString());
+        }
+        
+        Console.WriteLine("Count: " + estoque.Count);
+        Console.WriteLine("Capacity: " + estoque.Capacity);
+
+        Object obj = (Object)estoque[0];
+
+       int x = estoqueordenado.BinarySearch(estoqueordenado,obj);
+    }
+}
